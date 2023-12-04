d111d467837eddefcd9c85033e0885f25b8633a0
ujwaladhikari <adhikaridujwal001@gmail.com>
2023-12-04 12:33:40 +0545

    Abstract concept

--- Class1.cs
+++ Class1.cs
@@ -4,6 +4,20 @@ namespace ConsoleApp4
 {
     internal abstract class Demo1
     {
+        public void Check()
+        {
+            Console.WriteLine("Just Checking");
+        }
 
+        public abstract void Checking();
+        
+    }
+
+    internal class Demo2 : Demo1
+    {
+        public override void Checking()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
